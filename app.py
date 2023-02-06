5973d695791536207787f3145e506bbc8ba8c8c1
Brice Hoareau <bricesh@yahoo.com>
2023-02-06 14:47:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ COMPLETIONS_API_PARAMS = {
     "model": COMPLETIONS_MODEL,
 }
 
-openai.api_key = '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'
+openai.api_key = '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'
 
 def search_documentation(df, user_query, n=3):
     user_query_embedding = openai.Embedding.create(input=user_query, model=EMBEDDING_MODEL)['data'][0]['embedding']
